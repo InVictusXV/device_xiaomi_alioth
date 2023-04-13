d0062be099c1be40dca667f0d4bf38d4c15e9e0c
Yohan Black <103707560+Yohan-Black@users.noreply.github.com>
2023-04-13 15:17:12 -0500

    Alioth: Welcome SkylineUI project

--- aosp_alioth.mk
+++ aosp_alioth.mk
@@ -1,6 +1,7 @@
 #
 # Copyright (C) 2021 The LineageOS Project
 # Copyright (C) 2022-2023 VoidUI Project
+# Copyright (C) 2022-2023 SkylineUI Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -9,13 +10,13 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common VoidUI stuff.
+# Inherit some common SkylineUI stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := lineage_alioth
+PRODUCT_NAME := aosp_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
@@ -27,11 +28,11 @@ BUILD_FINGERPRINT := POCO/alioth_global/alioth:13/TKQ1.220829.002/V14.0.4.0.TKHM
 
 # Rom Specific Flags
 TARGET_INCLUDE_WIFI_EXT := true
-TARGET_BOOT_ANIMATION_RES := 1440
+TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
 TARGET_SUPPORTS_QUICK_TAP  := true
 
-# VoidUI Maintainer Flags
-VOID_MAINTAINER := johnmart19
+# SkylineUI Maintainer Flags
+SKYLINEUI_MAINTAINER := @Yohan_Black
 CUSTOM_BUILD_TYPE := OFFICIAL
