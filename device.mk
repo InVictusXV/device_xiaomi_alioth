4f9de35fa7a4e32fa7fccaeea1a295504c8d2660
Eidoron1 <eidoronp67@gmail.com>
2023-10-28 00:18:39 +0530

    alioth: Add support for miui camera

--- device.mk
+++ device.mk
@@ -29,6 +29,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libpiex_shim
 
+# Miuicamera
+$(call inherit-product-if-exists, vendor/xiaomi/camera/products/miuicamera.mk)
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
