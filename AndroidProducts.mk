c5ce8762475cd61aa3ac3e413beb5400039f34bc
InVictusXV <aradheakiki123@gmail.com>
2024-01-02 22:37:20 +0700

    alioth: Let's begin the Evolution

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_alioth.mk
+    $(LOCAL_DIR)/evolution_alioth.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_alioth-user \
-    derp_alioth-userdebug \
-    derp_alioth-eng
+    evolution_alioth-user \
+    evolution_alioth-userdebug \
+    evolution_alioth-eng
