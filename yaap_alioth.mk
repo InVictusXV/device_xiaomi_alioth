808ff435e4f175c10536a1b03985d337fc0cf12f
InVictusXV <aradheakiki123@gmail.com>
2024-10-24 10:23:03 +0700

    alioth: Yaap bringup

--- yaap_alioth.mk
+++ yaap_alioth.mk
@@ -8,22 +8,17 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common PixelOs stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Yaap stuff.
+$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
 
 # Device Specific Flags
-TARGET_INCLUDE_WIFI_EXT := true
 TARGET_BOOT_ANIMATION_RES := 1080
-
-# Environment Flags
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_INCLUDE_STOCK_ARCORE := true
-TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_BUILD_GAPPS=true
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := aosp_alioth
+PRODUCT_NAME := yaap_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
