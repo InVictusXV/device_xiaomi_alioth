fd63c5ad8e931d8455d55b8ca471def28ef62f5d
johnmart19 <johnivan19999@gmail.com>
2024-10-29 16:43:01 +0200

    Revert "alioth: Rename prebuilt audio HAL and libs"
    This reverts commit bf4c662ab69757f9dc4014622a39e36af76a78d8.
    
    Also, partially reverts 2f898dfe22542c546e66cb652ba056f6b7402f6f.

--- extract-files.sh
+++ extract-files.sh
@@ -21,10 +21,6 @@ function blob_fixup() {
 LEGACY_MIFARE_READER=1
 EOF
             ;;
-        vendor/lib/hw/audio.primary.alioth.so)
-            [ "$2" = "" ] && return 0
-            sed -i "s|/vendor/lib/liba2dpoffload\.so|liba2dpoffload_alioth\.so\x00\x00\x00\x00\x00" "${2}"
-            ;;
         vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
             [ "$2" = "" ] && return 0
             "${SIGSCAN}" -p "9A 0A 00 94" -P "1F 20 03 D5" -f "${2}"
