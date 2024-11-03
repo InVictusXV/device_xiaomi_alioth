1d76bc484cfd153fbc332cb624075f0a8eee57dd
InVictusXV <aradheakiki123@gmail.com>
2024-11-03 21:09:05 +0700

    alioth: Let's start the Derpness

--- lineage_alioth.mk
+++ lineage_alioth.mk
@@ -8,13 +8,16 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common DerpFeste stuff.
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
 
-PRODUCT_NAME := lineage_alioth
+DERP_BUILDTYPE := Kyrielight
+TARGET_BOOT_ANIMATION_RES := 1440
+
+PRODUCT_NAME := derp_alioth
 PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
