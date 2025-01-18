4af844315a5e293dbdc36f10f9af12b359454f8b
InVictusXV <aradheakiki123@gmail.com>
2025-01-18 23:12:07 +0700

    alioth: Let's start the Evolution

--- device.mk
+++ device.mk
@@ -39,7 +39,7 @@ PRODUCT_PACKAGES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aosp
+    $(LOCAL_PATH)/overlay-evolution
 
 PRODUCT_PACKAGES += \
     AliothFrameworks \
